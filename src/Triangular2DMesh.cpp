62399763a9e0337ae9b2d156c3d57f088a0873bb
Andrea Martelloni <a.martelloni@qmul.ac.uk>
2020-05-05 18:41:10 +0100

    Some code written based on current ideas

---
+++ src/Triangular2DMesh.cpp
@@ -0,0 +1,102 @@
+/**
+ * @file Triangular2DMesh.cpp
+ * @author Andrea Martelloni (a.martelloni@qmul.ac.uk)
+ * @brief 
+ * @version 0.1
+ * @date 2020-05-04
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include "Triangular2DMesh.hpp"
+#include <bitmask>
+
+
+#define FOREACH_MESH_POINT(expr) \
+    for (unsigned int c = 0; c < pi_.y_size; c++) { \
+        for (unsigned int k = 0; \
+            k < (pi_.x_size << 1) + !(c & 0x1)); \ // Even columns have one more element
+            k++) { expr }
+
+
+void Triangular2DMesh::GetInternalProperties(
+    Properties &p, PropertiesInternal &pi_) {
+
+    // Calculate how many interleaved mesh projections each axis
+    unsigned int x_size = std::ceilf(2.f * p.x__mm / (p.spatial_res__mm));
+    unsigned int y_size = std::ceilf(2.f * p.y__mm / (std::sqrtf(3.f) * p.spatial_res__mm));
+    // Make sure X is odd, Y is even
+    pi_.x_size = x_size + !(x_size & 0x1);
+    pi_.y_size = y_size + (y_size & 0x1);
+    pi_.total_size = pi_.x_size * pi_.y_size;
+}
+
+
+size_t Triangular2DMesh::GetMemSize(
+    Triangular2DMesh::Properties &p) {
+
+    PropertiesInternal pi;
+    GetInternalProperties(p, pi);
+    // Multiply by number of meshes needed
+    unsigned int num_size_by_meshes = num_size * 2;
+    // Return number of bytes
+    return num_size_by_meshes * sizeof(float);
+}
+
+
+Triangular2DMesh::Triangular2DMesh(Properties &p, void * mem) :
+    p_(p) {
+
+    GetInternalProperties(p, pi_);
+    // Stagger the mesh pointers:
+    // First V mesh - odd indexes
+    meshv_[0] = static_cast<float>(mem);
+    // Second V mesh - even indexes
+    meshv_[1] = meshv_[0] + 1;
+    // Third V mesh - next mesh group
+    meshv_[2] = meshv_[0] + pi_.total_size;
+    // Fourth V mesh - even indexes as int
+    mesh_mask_ = static_cast<uint32_t>(meshv_[2] + 1);
+    Reset();
+
+}
+
+void Triangular2DMesh::Reset() {
+
+    // Reset mesh offset to initial position
+    meshv_offset_ = 0;
+    float *v = VHist_(0);
+    float *v_z1 = VHist_(1);
+    float *v_z2 = VHist_(2);
+
+    // Set all current and previous meshes to 0
+    FOREACH_MESH_POINT(
+        // Clear all masks
+        SetV_(v, c, k, 0);
+        SetV_(v_z1, c, k, 0);
+        SetV_(v_z2, c, k, 0);
+    );
+}
+
+
+void Triangular2DMesh::ApplyMask(FormatMaskFn_type mask_fn) {
+    float x, y;
+    FOREACH_MESH_POINT(
+        CKtoXY_(c, k, x, y);
+        // If point itself is outside the mask, then it shouldn't
+        // receive any data
+        bool result = std::bitmask(0);
+        if (mask_fn(x, y)) {
+            // Otherwise, choose correct boundary function based on
+            // how many points in hexagon lie within the mask
+            // And build a bitmask from it.
+            // Bit 1: c-1, k (top left)
+            // Bit 2: c-1, k+1 (top right)
+            // Bit 3: c, k-1 (left)
+            // Bit 4: c, k+1 (right)
+            // Bit 5: c+1, k (bottom left)
+            // Bit 5: c+1, k+1 (bottom right)
+        }
+    );
+}
---
+++ src/Triangular2DMesh.hpp
@@ -0,0 +1,81 @@
+/**
+ * @file Triangular2DMesh.hpp
+ * @author Andrea Martelloni (a.martelloni@qmul.ac.uk)
+ * @brief 
+ * @version 0.1
+ * @date 2020-05-04
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#ifndef __TRIANGULAR_2D_MESH_HPP__
+#define __TRIANGULAR_2D_MESH_HPP__
+
+
+#include <cstddef>
+#include <cstdint>
+#include <cmath>
+
+static_assert(sizeof(float) == sizeof(uint32_t),
+    "Float needs to be exactly 32 bit for this code to work");
+
+
+class Triangular2DMesh {
+
+ public:
+
+    struct Properties {
+        float x__mm;
+        float y__mm;
+        float spatial_res__mm;
+    };
+    typedef bool (*FormatMaskFn_type)(float, float);
+
+    static size_t GetMemSize(Properties &p);
+    Triangular2DMesh(Properties &p, void *mem);
+    void Reset();
+    void ApplyMask(FormatMaskFn_type mask_fn);
+    void Process(float *input, float *output, unsigned int n_samples);
+
+ protected:
+
+    constexpr float kSqrt3Over2 = std::sqrtf(3.f) / 2.f;
+    struct Properties_internal_ {
+        unsigned int x_size;
+        unsigned int y_size;
+        unsigned int total_size;
+    };
+    Properties p_;
+    Properties_internal_ pi_;
+    float *meshv_[3];
+    unsigned int meshv_offset_;
+    uint32_t *mesh_mask_;
+
+    static void GetInternalProperties(Properties &p,
+        PropertiesInternal &pi_);
+    static __attribute__((always_inline)) float GetV_(float *v,
+        unsigned int c, unsigned int k) {
+        return v[c][2 * k + (c & 0x1)];
+    }
+    static __attribute__((always_inline)) void SetV_(float *v,
+        unsigned int c, unsigned int k, float value) {
+        v[c][2 * k + (c & 0x1)] = value;
+    }
+    __attribute__((always_inline)) float *VHist_(unsigned int z) {
+        unsigned int index = (meshv_offset_ + z);
+        while (index >= 3) {
+            index -= 3;
+        }
+        return meshv_[index];
+    }
+    __attribute__((always_inline)) void CKtoXY_(unsigned int c,
+        unsigned int k, float &x, float &y) {
+        y = static_cast<float>(c) * kSqrt3Over2 * p_.spatial_res__mm;
+        x = static_cast<float>(k) * p_.spatial_res__mm +
+            (p_.spatial_res__mm) * static_cast<float(c & 0x1); 
+    }
+};
+
+
+#endif
